e93e58ada20420261b8d7687c14b229edf03a988
Liran Schour <lirans@il.ibm.com>
2025-07-27 11:32:18 +0000

    squash

--- lmcache/v1/storage_backend/local_cpu_backend.py
+++ lmcache/v1/storage_backend/local_cpu_backend.py
@@ -483,8 +483,8 @@ class LocalCPUBackend(StorageBackendInterface):
 
             while len(notifs) == 0:
                 notifs = self._agent.get_new_notifs()
-                assert len(notifs) == 1
 
+            assert len(notifs) == 1. f"notifs len error = {len(notifs)}"
             print(f"XXX REMOVE ME got response {len(notifs)}")
 
         # NIXL PUSH END
