941cccbe03118c9e1e44b7d3dae1965d22bc7e39
Liran Schour <lirans@il.ibm.com>
2025-07-29 13:26:57 +0000

    Sync for NIXL transfer completion only when accessing memory_objs at receiver side

--- lmcache/v1/storage_backend/local_cpu_backend.py
+++ lmcache/v1/storage_backend/local_cpu_backend.py
@@ -336,6 +336,9 @@ class LocalCPUBackend(StorageBackendInterface):
                     skip_desc_merge=False,  # XXX need to check this
                 )
 
+                for mem_obj in memory_objs:
+                    mem_obj.metadata.handle = handle
+
                 self.insert_transfer(handle)
 
                 # Begin async xfer.
@@ -354,7 +357,7 @@ class LocalCPUBackend(StorageBackendInterface):
                 # self._agent.release_xfer_handle(handle)
                 # self._active_transfers.pop(handle, None)
 
-                self.wait_for_transfer(handle)
+                #self.wait_for_transfer(handle)
 
                 end = time.perf_counter()
                 logger.info(f"========== TRANSFER completed in {end - start} {(len(local_descs_ids) * self._nixl_block_size)/((end - start) * (1 << 30)):.3f} GB/s")
@@ -453,11 +456,10 @@ class LocalCPUBackend(StorageBackendInterface):
         pushed_keys = []
         for key, memory_obj in zip(keys, memory_objs, strict=False):
             if memory_obj.get_shape()[2] % self._nixl_chunk_size == 0:
-                print(f"XXX Send partial keys len={len(metadatas)} keys len = {memory_obj.get_shape()[2]}")
                 metadatas.append(memory_obj.metadata)
                 pushed_keys.append(key)
             else:
-                print(f"XXX skip {memory_obj.get_shape()[2]}")
+                print(f"XXX skip not aligned chunk size {memory_obj.get_shape()[2]}")
             self.submit_put_task(key, memory_obj)
 
         if self._nixl_role == "sender":
@@ -468,12 +470,12 @@ class LocalCPUBackend(StorageBackendInterface):
             self._side_channel.send(data)
             logger.info("Sent the request with %d keys and waiting for ack by notif", len(pushed_keys))
 
-            notifs = self._agent.get_new_notifs()
+            # notifs = self._agent.get_new_notifs()
 
-            while len(notifs) == 0:
-                notifs = self._agent.get_new_notifs()
+            # while len(notifs) == 0:
+            #     notifs = self._agent.get_new_notifs()
 
-            assert len(notifs) == 1, f"notifs len error = {len(notifs)}"
+            # assert len(notifs) == 1, f"notifs len error = {len(notifs)}"
 
         # NIXL PUSH END
 
@@ -499,6 +501,12 @@ class LocalCPUBackend(StorageBackendInterface):
             # is evicted from the local cpu backend before the caller calls
             # ref count up themselves
             memory_obj.ref_count_up()
+
+            handle = memory_obj.metadata.handle
+            if handle is not None:
+                self.wait_for_transfer(handle)
+                memory_obj.metadata.handle = None
+
             self.hot_cache.move_to_end(key)
             return memory_obj
 
