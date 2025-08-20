75bb27c01527f5aba64c3e62646a2d8724b853fc
Liran Schour <lirans@il.ibm.com>
2025-08-20 04:23:07 +0000

    n/a

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -905,6 +905,10 @@ class LMCacheConnectorV1Impl:
                 f"{self.load_specs[request.request_id].vllm_cached_tokens}"
                 f" for request {request.request_id}"
             )
+        if params is not None and params.get("do_remote_prefill"):
+            # Only trigger 1 KV transfer per request.
+            params["do_remote_prefill"] = False
+
         logger.info(f"XXX {request}")
         self.load_specs[request.request_id].can_load = True
 
