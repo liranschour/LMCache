6422ed91dd58c5e8190590ee7233930a87e9056f
Liran Schour <lirans@il.ibm.com>
2025-08-24 12:54:49 +0000

    n/a

--- lmcache/v1/storage_backend/local_cpu_backend.py
+++ lmcache/v1/storage_backend/local_cpu_backend.py
@@ -243,39 +243,40 @@ class LocalCPUBackend(StorageBackendInterface):
                         del self._transfers[req_id]
             else:
                 # WRITE
-                with self._transfers_lock:
-                    for req_id, (handle, t_done, msg_size, start) in self._transfers.items():
-                        state = self._agent.check_remote_xfer_done(self.peer_name, handle.encode())
+                # with self._transfers_lock:
+                #     for req_id, (handle, t_done, msg_size, start) in self._transfers.items():
+                #         state = self._agent.check_remote_xfer_done(self.peer_name, handle.encode())
 
-                        if state == "ERR":
-                            print("Transfer got to Error state.")
-                            exit()
-                        elif state == "DONE":
-                            end = time.perf_counter()
-                            logger.info(f"========== TRANSFER completed:  {msg_size/(1<<20):.2f} MB BW: {msg_size/((end - start) * (1 << 30)):.3f} GB/s")
-
-                            t_done.set()
-                            finished_transfer.append(req_id)
-
-                    for req_id in finished_transfer:
-                        assert req_id in self._transfers
-                        del self._transfers[req_id]
-
-                # all_notifs = self._agent.get_new_notifs().values() # XXX the code assumes a single peer for now
-                # for notifs in all_notifs:
-                #     for notif in notifs:
-                #         handle = notif.decode("utf-8")
-
-                #         value = None
-                #         with self._transfers_lock:
-                #             value = self._transfers.pop(handle, None)
-
-                #         if value is not None:
-                #             t_done, msg_size, start = value
+                #         if state == "ERR":
+                #             print("Transfer got to Error state.")
+                #             exit()
+                #         elif state == "DONE":
                 #             end = time.perf_counter()
                 #             logger.info(f"========== TRANSFER completed:  {msg_size/(1<<20):.2f} MB BW: {msg_size/((end - start) * (1 << 30)):.3f} GB/s")
 
                 #             t_done.set()
+                #             finished_transfer.append(req_id)
+
+                #     for req_id in finished_transfer:
+                #         assert req_id in self._transfers
+                #         del self._transfers[req_id]
+
+                all_notifs = self._agent.get_new_notifs().values() # XXX the code assumes a single peer for now
+                for notifs in all_notifs:
+                    for notif in notifs:
+                        print(f"XXXX {notif}")
+                        handle = notif.decode("utf-8")
+
+                        value = None
+                        with self._transfers_lock:
+                            value = self._transfers.pop(handle, None)
+
+                        if value is not None:
+                            t_done, msg_size, start = value
+                            end = time.perf_counter()
+                            logger.info(f"========== TRANSFER completed:  {msg_size/(1<<20):.2f} MB BW: {msg_size/((end - start) * (1 << 30)):.3f} GB/s")
+
+                            t_done.set()
 
             time.sleep(0.001)  # Avoid busy waiting
 
